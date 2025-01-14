2d01b699399b8162636a939db53e2de4e5be8fd0
Lucas N. Pinto <123923498+lnpinto@users.noreply.github.com>
2025-01-14 17:12:13 -0300

    Add files via upload

---
+++ Aeneid_text-based_game.py
@@ -0,0 +1,192 @@
+def main():
+    print('-' * 85, '\n', ' ' *30, 'ESCAPE FROM TROY')
+    print('-' * 85)
+    
+    # Intro
+    
+    print(' ')
+    print(' In the tenth year of the Trojan War, the Greeks constructed an enormous wooden horse,\n which they then rumored was intended as an offering to the goddess Minerva in order\n to gain her protection on their voyage home. In truth, they filled the horse with warriors,\n and then hid themselves in their ships behind the offshore island of Tenedos...')
+    press_enter()
+    # print('Destiny’s engine, pregnant with weapons, mounts the walls. Round about unmarried boys\n and girls sing hymns, and take delight in laying hands on the rope.\n The Horse ascends and, menacingly, glides into the centre of the city.')
+    print(" So the sky revolves and night rushes over the Ocean, wreathing the earth, the vault\n of heaven and the trickery of the Greeks in a great darkness. Relaxed within their walls,\n the Trojans fell silent. Sleep embraced their tired limbs. Now the Greek infantry\n in well-marshalled ships was coming from Tenedos, through the cooperative silence\n of the unspeaking moon, making for the shore it knew; King Agamemnon’s ship had raised the signal,\n and under the protection of the gods’ unfair destiny, the Greeks were secretly\n released from the Horse’s womb, and so were the bars of pine. The Horse was opened up\n and released them to the air and they emerge from the hollowed timber, delighted...")
+    press_enter()
+    print(" They invade a city weighed down by sleep and wine. The guards are butchered\n and through the open gates they let in all their comrades and marshal all the conspiring Greeks...")
+    press_enter()
+    
+    # Início do loop 
+    # while True:
+
+    print(" It was the hour when first sleep began for poor mortals and by\n divine gift crept on most welcome. Hector appears to Aeneas in his sleep\n and says heavily drawing groans from the depths of his breast:\n\n Alas! Flee, son of the goddess. Tear yourself from these flames;\n the enemy holds our walls and Troy collapses from her lofty roof-trees down...")
+    press_enter()
+    player_answer = int(input(" What should Aeneas do?\n 1 - Go back to sleep.\n 2 - Get up and see what's going on. "))
+    print(' ')
+    
+    if player_answer == 2:
+        print(" Shaken from sleep, he climbs and crosses the highest tree of the roof to stand with ears pricked.\n Then what the Greeks had really done was proved and their trickery stood revealed.")
+        press_enter()
+    elif player_answer == 1:
+        print(" Smoke and flames engulfed the room during Aeneas' sleep.")
+        the_end()
+        quit()
+                                   
+    print(" A shouting of men and the bray of trumpets arises. Maddened, Aeneas takes up arms,\n yet there is not enough plan in those arms, but rather his\n heart is fired to gather together a handful of men for the fight and\n to concentrate on the citadel with his companions. Frenzy and rage\n overrun his mind, and the thought comes that it is a fine thing to die under arms...")    
+    print(' ')
+    player_answer_2 = int(input(" Aeneas leaves his house. To the right there is a dark and quiet street. To the left he sees flames and hears the distant sounds of battle.\n Where should Aeneas head next?\n 1 - Go to the right.\n 2 - Go to the left. "))
+    print(' ')
+    
+    if player_answer_2 == 1:
+        print(" A line of steel with flashing points drawn stands there ready to kill. Aeneas has been ambushed by Greek soldiers.")
+        the_end()
+        quit()
+    elif player_answer_2 == 2:
+        print(" Aeneas is carried into the flames and into the fray, where the grim Erinys,\n where the clamour calls and the shouting which reaches to the skies.\n Other soldiers join him in battle.")
+        press_enter()
+        
+    print(" And Aeneas says:\n\n Lads, hearts that are so brave to no end, if your wish\n is steadfast to follow me as I dare the very worst, you see what chance\n there is in the situation. The gods by whose will this realm had\n stood have all quit their altars and shrines and are gone. You are\n bringing succour to a city in flames. Let us die and let us rush into\n the midst of the fighting. The only salvation for the vanquished is not\n to hope for any survival...")
+    press_enter()
+    player_answer_3 = int(input(" What should Aeneas do next?\n 1 - Try to reach the city walls.\n 2 - Head to the centre of the city. "))
+    print(' ')
+    if player_answer_3 == 1:
+        print(" On his way to the city walls, Aeneas was captured and taken as prisioner by Greek soldiers.")
+        the_end()
+        quit()
+    elif player_answer_3 == 2:
+        print(" Through fire and fighting they go towards a certain\n death and keep to the way through the centre of the city. Black night\n hovers round them with her dome of shadow.")
+    press_enter()
+    # print(" An ancient city, which had ruled for many years, is falling.\n Countless bodies are strewn helpless throughout the streets,\n the houses, and the hallowed portals of the gods...")
+    # press_enter()
+    print(" Everywhere there is harsh tragedy, everywhere fear and countless visions of death.\n Of the Greeks, Androgeos was the first to face them, with a great following at his back,\n foolishly believing them a unit of allies, and actually addressed them with friendly words:\n\n Hurry, men! Just what idleness has kept you so late? Others are\n sacking and pillaging Troy’s citadel in flames, while only now are you\n arriving from the lofty ships?")
+    press_enter()
+    player_answer_4 = int(input(" Aeneas and the Trojans have been discovered! What should they do?\n 1 - Pretend to be part of the Greek army.\n 2 - Rush in and attack Androgeos and the Greek soldiers. "))
+    print(' ')
+    if player_answer_4 == 1:
+        print(" As no sufficiently convincing answer was given, he realised that\n he had fallen into the midst of the enemy and killed Aeneas and the Trojan soldiers. ")
+        the_end()
+        quit()
+    elif player_answer_4 == 2:
+        print(" Just so Androgeos, appalled at the sight, made to withdraw.\n The Trojans rushed in and, close-packed under arms, poured round them.\n Ignorant as they were of the spot and prey to fear, the Trojans laid them low.\n Luck smiled on the beginning of their enterprise...")
+        press_enter()
+
+    print(" Now Coroebus, glorying in his success and in his courage, said:\n\n Comrades, where our luck reveals the path of survival and\n where she manifests herself as favourable, let us follow up.\n Let us change shields and fit ourselves with Greek insignia. Trickery or courage?\n Who would enquire, in the case of an enemy? They will supply the weapons, themselves...")
+    press_enter()
+    print(" They go on, mingled with the Greeks, under a protection not favourable to them.\n They clash and fight many skirmishes, through the unseeing night\n and send many a Greek down to Orcus...")
+    press_enter()
+    print(" Alas! No man has the right to trust in the gods when they are against him.\n See! Priam’s maiden daughter Cassandra, with hair outspread, was being dragged\n from the sanctum of Minerva’s temple, straining her blazing eyes\n towards heaven to no avail, her eyes, for bonds constrained her soft hands.\n This sight Coroebus, with his wits maddened, could not bear and rushed\n doomed into the midst of their ranks...")
+    press_enter()
+        
+    player_answer_5 = int(input(" Will Aeneas go after Coroebus and try to save Cassandra?\n 1 - Yes\n 2 - No "))
+    print(' ')
+    
+    if player_answer_5 == 1:
+        print(" The Trojans followed and charged with close-packed weapons. Here first from the\n temple’s high roof they are crushed by their own side’s arms and\n a most pitiable slaughter starts, on account of their armour’s appearance\n and through a confusion about the Greek plumes.")
+        press_enter()
+        print(" Then the Greeks, with an angry bellow at the maiden’s\n being snatched away gather from all sides and attack, Ajax, fiercest of warriors,\n and the two sons of Atreus and the whole contingent of the Dolopians...")
+        press_enter()
+        print(" Those Greeks too whom they dispersed amid the dark shadows of the\n night reappear and are the first to recognise the deceiving shields and weapons\n and take note of their voices, different in timbre. At the last, they are overborne by numbers...")
+        press_enter()
+        print(" Aeneas manages to escape with Iphitus and Pelias, following the screams that come straight from Priam’s palace.")
+        press_enter()
+    elif player_answer_5 == 2:
+        print(" Coroebus ran after Cassandra as Aeneas and the Trojans moved on to Priam's palace.")
+        press_enter()
+        
+    player_answer_6 = int(input(" The Trojans finayly arrive at Priam's palace. Will they go inside?\n 1 - Yes\n 2 - No "))
+    print(' ')
+
+    if player_answer_6 == 1:
+        print( " Aeneas' courage is renewed, to come to the palace’s help, to relieve\n the defenders by his aid and to lend force to the vanquished.")
+        press_enter()
+        print(" There was a doorway, and an entrance, unseen, and a passage right\n through Priam’s palace, from one end to the other and a neglected\n door which Aeneas uses to enter the palace...")
+        press_enter()
+        print(" At the front of the forecourt, on the fore-edge of the step, Pyrrhus\n revels, a-glitter with the brazen gleam of his arms. In the front rank,\n he seized a double axe and is trying to break down the stout gates\n and to shift the bronze-clad jambs from their sockets...")
+        press_enter()
+        print(" He has cut through a balk of wood and has hollowed his way\n through the solid oak and has created a great hole with a wide mouth.\n The palace within is revealed, and the long courtyards appear, Priam’s\n sanctum and that of the old kings too, and he catches sight of the\n armed guards standing on the threshold’s edge...")
+        press_enter()
+        
+        player_answer_7 = int(input(" Throw the spear at Pyrrhus?\n 1 - Yes\n 2 - No "))
+        print(' ')
+        if player_answer_7 == 1:
+            print(" Aeneas tried to hit Pyrrhus with his spear, but he deflected it with his bronze shield.")
+            press_enter()
+        elif player_answer_7 == 2:
+            print(" Aeneas thought it was better not to call Pyrrhus attention and did not throw the spear at him.")
+            press_enter()
+        
+        print(" Pyrrhus cut through the guards and chased Polites, one of Priam’s sons, at hostile spear-point.\n When he finally reached him, he pressed him close with his spear...")
+        press_enter()
+        print(" Polites collapsed and poured away his life in a flood of blood.\n Now Priam, though grasped in the very midst of death, did not hold back,\n nor spared his angry voice:\n\n For such a crime, for such an outrage, may the gods grant you\n due reward, if there is any pity in heaven to see to such things. May they pay you\n the price due and render you a proper reward, you, who made me see\n before my eyes my son’s death, and polluted a father’s gaze with his end...")        
+        press_enter()
+        print(" Pyrrhus dragged Priam to the great altar in the middle of the palace,\n shaking and slipping in his son’s copious blood. He wound the\n king’s hair in his left hand and with his right unsheathed his glittering\n sword and plunged it in Priam’s side up to the hilt. This was the end of\n Priam’s destiny, this death carried him off by his lot, as he beheld\n Troy fired and Pergama in ruins, Priam once proud ruler of Asia over\n so many peoples and lands...")
+        press_enter()
+        print(" A vision of Aeneas' dear father came to him as he saw the king,\n his contemporary, breathing out his life thanks to a cruel wound;\n Creusa came to him, all alone as she was, and his house sacked\n and the fate of their little son, Iulus...")
+        press_enter()
+        print(" He glances behind him and looks over what force is still about him.\n They have all left him in their exhaustion and with a jump have cast\n their bodies to the ground, or given them, exhausted, to the flames.\n And so Aeneas decides to leave the palace in ruins...")
+        press_enter()
+    elif player_answer_6 == 2:
+        print(" The entrance of the palace is blocked and Aeneas decides to move forward.")
+        press_enter()
+        
+    print(" Aeneas goes down, and with Venus to guide him, finds his way between flame and foe;\n weapons give way and flames fall back. And when the threshold of his father’s home,\n his ancient dwelling, was reached, his father, whom he wanted and sought\n above all to carry off to the high mountains, now refused\n to prolong his life and to tolerate exile, once Troy was demolished...")
+    press_enter()
+    print(" Then with a sudden burst of noise it thundered on the left, and slipping down\n through the gloom from the sky, a meteor rushed with a strong light,\n followed by its trail. They saw it falling over the tops of the palace roofs,\n and plunging brightly into the forests of Ida...")
+    press_enter()
+        
+    player_answer_8 = int(input(" Follow the track of the meteor?\n 1 - Yes\n 2 - No "))
+    print(' ')
+
+    if player_answer_8 == 1:
+        print(" Aeneas spread over his broad shoulders and over the neck he sets below his father\n a covering of the skin of a tawny lion. Little Iulus grasps closely his right hand and\n follows his father with unequal paces. His wife comes on behind. They move\n through the darkest areas. Every breath of air now alarms, every sound agitates in his uncertainty,\n as he fears alike for his companion and for his burden...")
+        press_enter()
+
+    elif player_answer_8 == 2:
+        print(" Aeneas decides to try to reach the west gate.")
+        press_enter()
+        print(" When they were drawing near the gate, and thought\n they had covered the whole distance, the sound of many feet reached their ears.\n Greek soldiers fall upon them and kill them all...")
+        the_end()
+        quit()
+        
+    print(" On their way to the point of Mount Ida where the meteor landed,\n Aeneas's father, looking out through the darkness, exclaimed:\n\n My son, flee, my son. They are close at hand. I see their flaming shields\n and the flicker of their bronze armour...")
+    press_enter()
+    print(" As Aeneas went at a run through an area without paths, and diverged from the familiar route,\n alas, whether his wife Creusa was swept off by fate as she stopped, or strayed from the path,\n or sat down exhausted, it is not clear, nor did she again appear before his eyes...")
+    press_enter()
+
+    player_answer_9 = int(input(" Should Aeneas go back and search for his wife?\n 1 - Yes\n 2 - No "))
+    print(' ')
+
+    if player_answer_9 == 1:
+        print(" Aeneas entrust his son Ascanius, his father Anchises and the household gods of Troy\n to his companions and conceal them in a curving valley and makes for the city.")
+        press_enter()
+        print(" He returns to the walls and follows back the track he had marked through the dark,\n and with his eyes he searches. He makes his way home, if only she, if only she has\n directed her steps there. The Greeks had burst in, and were occupying the whole building.\n Worst of all, a devouring fire is rolled up to the highest rooftrees by the wind;\n the flames leap above them and the blaze rages to the skies. He goes on, and return\n to Priam’s seat on the citadel...")
+        press_enter()
+        print(" Aeneas called on her over and over again through the darkness, but to no avail.\n Then, the tragic wraith and spectre of Creusa herself appeared before his eyes.\n He was astonished; his hair stood on end, and his voice stuck in his throat...")
+        press_enter()
+        print(" Then she spoke:\n\n Why are you so keen to indulge in crazed grief, my dear husband?\n Yours is a long exile, yours the waste of ocean’s surface to plough,\n and you will come to the land of Hesperia, where the Lydian Tiber,\n between the inhabitants’ fertile meadows, flows with a gentle advance.\n There you will secure prosperous circumstances, a realm, and a royal wife.\n Set aside tears for your beloved Creusa. Now, farewell, and do you preserve\n the love of our common son...")
+        press_enter()
+        print(" Three times he tried to put his arms about her neck and three times her wraith,\n though embraced to no avail, slipped through his hands like the insubstantial winds\n and very similar to a swift dream. It was thus that he finally saw again\n his comrades as the night ended.")
+        press_enter()
+
+    elif player_answer_9 == 2:
+        print(" Aeneas did not look back to see if she was lost, nor did give her thought\n before they reached the old mound and hallowed sanctuary of Ceres.\n There at last they all gathered; she alone was absent and disappointed companions,\n son and husband.")
+        press_enter()
+        
+    print(" And now a great number of new companions had flowed together,\n both men and women, a force gathered for exile, a crowd deserving pity.\n They had come together from all quarters, ready with means and will\n to follow into whatever lands Aeneas might wish to conduct them over the sea...")
+    press_enter()
+    print(" The Dawn-star was rising from the ridges at the top of Mount Ida and was\n bringing the day. The Greeks were holding the entrances of the gates\n under guard and no hope of succour was offered. Aeneas gave way, picked up\n his father and made for the mountain.")
+    the_end()
+    quit()
+        
+        
+def press_enter():
+    print(' ')
+    print('Press enter to continue... ')
+    input()
+    
+
+def the_end():
+    print(' ')
+    print(' THE END')
+    print(' ')
+
+
+if __name__ == '__main__':
+    main()
